1374c9cee7f87296635255e57a00d1a5c5a6fe1c
Gabriel Kaminise <gabriel.misao08@gmail.com>
2025-09-04 16:33:36 -0300

    Ajustes

--- src/main/java/com/example/ManagerProjectPDS/ManagerPdsProjectApplication.java
+++ src/main/java/com/example/ManagerProjectPDS/ManagerPdsProjectApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PsicoManagerProjectApplication {
+public class ManagerPdsProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PsicoManagerProjectApplication.class, args);
+		SpringApplication.run(ManagerPdsProjectApplication.class, args);
 	}
 }
